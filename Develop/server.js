44b822b0af99b221faad549b071578d2fe79b2d7
Rajnil Prasad <rajnilprasad.rp@gmail.com>
2020-07-02 14:36:30 -0700

    initial commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,13 @@
 const   express = require("express");
 
 // import our routes 
-const apiRoutes = require("../Routes/apiRoutes");
-const htmlRoutes = require("../Routes/htmlRoutes");
+const apiRoutes = require("./Routes/apiRoutes");
+const htmlRoutes = require("./Routes/htmlRoutes");
 
 // inantiate our express app
 const app = express ();
 
-const Port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // boiler plate code to make api calls work
 app.use(express.urlencoded({ extended: true }));
